697dd20bfbf2a8e19ba635995a67d6bebacf1a9e
Yaser Jarrar <yaser@grout.io>
2014-09-28 06:33:07 +0400

    Use 'mylar' namespace for dependent mylar packages

--- package.js
+++ package.js
@@ -7,8 +7,8 @@ Package.on_use(function(api) {
     api.use('accounts-base', ['client', 'server']);
     api.use('accounts-password', ['client', 'server']);
     api.use('srp', ['client', 'server']);
-    api.use('basic-crypto', ['client', 'server']);
-    api.use('principal', 'client');
+    api.use('mylar:basic-crypto', ['client', 'server']);
+    api.use('mylar:principal', 'client');
     
     api.add_files('accounts_common.js', ['client','server']);
     api.add_files('idp_client.js', ['client', 'server'])
