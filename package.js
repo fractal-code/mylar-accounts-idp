ded5e0612b1ba0b71259c5d4e4511657036f5326
Yaser Jarrar <yaser@grout.io>
2014-10-04 19:55:46 +0400

    Update package def syntax for Meteor 0.9

--- package.js
+++ package.js
@@ -2,19 +2,15 @@ Package.describe({
   summary: "Login service for IDP accounts"
 });
 
-Package.on_use(function(api) {
-
-    api.use('accounts-base', ['client', 'server']);
-    api.use('accounts-password', ['client', 'server']);
-    api.use('srp', ['client', 'server']);
-    api.use('mylar:basic-crypto', ['client', 'server']);
+Package.onUse(function (api) {
+    api.use(['accounts-base', 'accounts-password', 'mylar:basic-crypto', 'srp'], ['client', 'server']);
     api.use('mylar:principal', 'client');
-    
-    api.add_files('accounts_common.js', ['client','server']);
-    api.add_files('idp_client.js', ['client', 'server'])
-    api.add_files('idp_token.js', 'client');
-    api.add_files('accounts_client.js', 'client');
-    api.add_files('accounts_server.js', 'server');
+
+    api.addFiles('accounts_common.js', ['client', 'server']);
+    api.addFiles('idp_client.js', ['client', 'server']);
+    api.addFiles('idp_token.js', 'client');
+    api.addFiles('accounts_client.js', 'client');
+    api.addFiles('accounts_server.js', 'server');
 
     // TODO: export just one variable
     api.export("idp_init");
