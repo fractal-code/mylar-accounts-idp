0e3bc5db7f7627df94597fec8ab2507309c433ce
Yaser Jarrar <yaser@grout.io>
2014-10-20 17:40:14 +0400

    Bump version to 0.1.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     summary: "Login service for IDP accounts",
     name: "mylar:accounts-idp",
-    version: "0.1.0",
+    version: "0.1.1",
     git: "https://github.com/gliesesoftware/mylar-accounts-idp.git"
 });
 
